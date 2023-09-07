6e62ebd22fb1c135cc682d3e4a63b17e98b004ae
Sceat <fetch@sceat.xyz>
2023-09-08 00:27:00 +0400

    feat: add support for subscription directives

--- index.js
+++ index.js
@@ -1,72 +1,110 @@
-import { defaultFieldResolver, buildSchema, getDirectiveValues } from 'graphql'
+import {
+  defaultFieldResolver,
+  buildSchema,
+  getDirectiveValues,
+  isObjectType,
+} from 'graphql'
 
 export default ({ document, resolvers = {}, directives = {} }) => {
   const built_schema = buildSchema(document, { noLocation: true })
 
   Object.entries(resolvers).forEach(([type_name, fields_handlers]) => {
     const type = built_schema.getType(type_name)
+
+    if (!isObjectType(type)) return // Skip if not an object type
+
     const fields = type.getFields()
 
     Object.entries(fields_handlers).forEach(([field_name, handler]) => {
       const field = fields[field_name]
-      const handler_type = handler?.constructor?.name
-
       if (!field) throw new Error(`${field_name} is not in schema`)
-      switch (handler_type) {
-        case 'GeneratorFunction':
-        case 'AsyncGeneratorFunction':
-          field.subscribe = handler
-          break
-        case 'AsyncFunction':
-        case 'Function':
-          field.resolve = handler
-          break
-        default: {
-          const { resolve, subscribe } = handler
 
-          field.resolve = resolve
-          field.subscribe = subscribe
-          break
-        }
+      field.resolve = defaultFieldResolver
+
+      if (
+        ['GeneratorFunction', 'AsyncGeneratorFunction'].includes(
+          handler.constructor.name
+        )
+      ) {
+        field.subscribe = handler
+      } else if (typeof handler === 'function') {
+        field.resolve = handler
+      } else {
+        if (handler.resolve) field.resolve = handler.resolve
+        if (handler.subscribe) field.subscribe = handler.subscribe
       }
     })
   })
 
-  const attach_directive = field => {
-    const node = field.astNode
-
-    field.resolve = node?.directives?.reduce((resolve, node_directive) => {
-      const {
-        name: { value: directive_name },
-      } = node_directive
-      const directive_resolver = directives[directive_name]
-
-      if (!directive_resolver) return resolve
-
-      const directive_arguments = getDirectiveValues(
-        built_schema.getDirective(directive_name),
-        node
-      )
-
-      return (root = {}, parameters = {}, context = {}, info) =>
-        directive_resolver({
-          resolve: async () => resolve(root, parameters, context, info),
+  const wrap_with_directive =
+    (original_function, directive_resolver, directive_arguments) =>
+    async (root = {}, parameters = {}, context = {}, info) => {
+      try {
+        const result = await directive_resolver({
+          resolve: async () =>
+            original_function(root, parameters, context, info),
           root,
           parameters,
           context,
           info,
           directive_arguments,
         })
-    }, field.resolve ?? defaultFieldResolver)
+
+        if (info.operation.operation !== 'subscription') return result
+
+        async function* wrap_in_async_iterable(value) {
+          yield value
+        }
+
+        return wrap_in_async_iterable(result)
+      } catch (error) {
+        if (info.operation.operation !== 'subscription') throw error
+
+        async function* yield_error() {
+          yield {
+            candles: () => {
+              throw error
+            },
+          }
+        }
+
+        return yield_error()
+      }
+    }
+
+  const attach_directive = field => {
+    const node = field.astNode
+
+    node?.directives?.forEach(node_directive => {
+      const directive_name = node_directive.name.value
+      const directive_resolver = directives[directive_name]
+
+      if (directive_resolver) {
+        const directive_arguments = getDirectiveValues(
+          built_schema.getDirective(directive_name),
+          node
+        )
+
+        if (field.subscribe)
+          field.subscribe = wrap_with_directive(
+            field.subscribe,
+            directive_resolver,
+            directive_arguments
+          )
+        else if (field.resolve)
+          field.resolve = wrap_with_directive(
+            field.resolve,
+            directive_resolver,
+            directive_arguments
+          )
+      }
+    })
   }
 
   Object.values(built_schema.getTypeMap())
-    .filter(({ name }) => name?.startsWith?.('__') === false)
+    .filter(type => isObjectType(type) && !type.name.startsWith('__'))
     .forEach(type => {
-      const fields = type?.getFields?.()
-
-      if (!fields) return
-
+      const fields = type.getFields()
       Object.values(fields).forEach(attach_directive)
     })
 
