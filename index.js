42b4e94f506bdd1c08fb17f3cbbc3a440708c646
Sceat <fetch@sceat.xyz>
2023-09-11 02:00:03 +0400

    fix: double wrapping

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ import {
 export default ({ document, resolvers = {}, directives = {} }) => {
   const built_schema = buildSchema(document, { noLocation: true })
 
+  const wrap_subscription_result = (resolver, field_name) => {
+    return async function* (...args) {
+      for await (const value of resolver(...args)) {
+        yield { [field_name]: value }
+      }
+    }
+  }
+
   Object.entries(resolvers).forEach(([type_name, fields_handlers]) => {
     const type = built_schema.getType(type_name)
 
@@ -23,57 +31,84 @@ export default ({ document, resolvers = {}, directives = {} }) => {
 
       if (
         ['GeneratorFunction', 'AsyncGeneratorFunction'].includes(
-          handler.constructor.name
+          handler.constructor.name,
         )
       ) {
-        field.subscribe = handler
+        field.subscribe = wrap_subscription_result(handler, field_name)
       } else if (typeof handler === 'function') {
         field.resolve = handler
       } else {
         if (handler.resolve) field.resolve = handler.resolve
-        if (handler.subscribe) field.subscribe = handler.subscribe
+        if (handler.subscribe) {
+          field.subscribe = wrap_subscription_result(
+            handler.subscribe,
+            field_name,
+          )
+        }
       }
     })
   })
 
-  const wrap_with_directive =
-    (original_function, directive_resolver, directive_arguments) =>
-    async (root = {}, parameters = {}, context = {}, info) => {
-      try {
-        const result = await directive_resolver({
-          resolve: async () =>
-            original_function(root, parameters, context, info),
+  const wrap_with_directive = (
+    original_function,
+    directive_resolver,
+    directive_arguments,
+    field_name,
+  ) => {
+    return async (...args) => {
+      const [root, parameters, context, info] = args
+
+      if (info.operation.operation !== 'subscription') {
+        // The original behavior for non-subscription operations
+        return await directive_resolver({
+          resolve: () => original_function(root, parameters, context, info),
           root,
           parameters,
           context,
           info,
           directive_arguments,
         })
-
-        if (info.operation.operation !== 'subscription') return result
-
-        async function* wrap_in_async_iterable(value) {
-          yield value
-        }
-
-        return wrap_in_async_iterable(result)
-      } catch (error) {
-        if (info.operation.operation !== 'subscription') throw error
-
-        async function* yield_error() {
-          yield {
-            candles: () => {
-              throw error
+      } else {
+        // If it's a subscription, the wrapping should return an async generator
+        async function* subscription_generator() {
+          const asyncIterable = await directive_resolver({
+            resolve: async function* () {
+              for await (const value of original_function(
+                root,
+                parameters,
+                context,
+                info,
+              )) {
+                yield value
+              }
             },
+            root,
+            parameters,
+            context,
+            info,
+            directive_arguments,
+          })
+
+          // Check if the directive resolver returns an asyncIterable.
+          if (Symbol.asyncIterator in Object(asyncIterable)) {
+            for await (const item of asyncIterable) {
+              yield item
+            }
+          } else {
+            throw new Error(
+              `Directive resolver must return an AsyncIterator when used on a Subscription.`,
+            )
           }
         }
 
-        return yield_error()
+        return subscription_generator()
       }
     }
+  }
 
   const attach_directive = field => {
     const node = field.astNode
+    const field_name = field.name
 
     node?.directives?.forEach(node_directive => {
       const directive_name = node_directive.name.value
@@ -82,20 +117,22 @@ export default ({ document, resolvers = {}, directives = {} }) => {
       if (directive_resolver) {
         const directive_arguments = getDirectiveValues(
           built_schema.getDirective(directive_name),
-          node
+          node,
         )
 
         if (field.subscribe)
           field.subscribe = wrap_with_directive(
             field.subscribe,
             directive_resolver,
-            directive_arguments
+            directive_arguments,
+            field_name,
           )
         else if (field.resolve)
           field.resolve = wrap_with_directive(
             field.resolve,
             directive_resolver,
-            directive_arguments
+            directive_arguments,
+            field_name,
           )
       }
     })
